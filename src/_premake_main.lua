004515b7c2ece1e4d0218290fbd197af9f8a3d6e
Richard Geary <richardg.work@gmail.com>
2012-07-25 13:06:24 -0400

    Added defaultaction
    --HG--
    branch : defaultaction

--- src/_premake_main.lua
+++ src/_premake_main.lua
@@ -166,3 +166,12 @@
 		return 0
 
 	end
+	
+	function defaultaction(osName, actionName)
+	   if (actionName == nil) then
+	     _ACTION = _ACTION or osName
+	   end	   
+	   if os.is(osName) then
+	      _ACTION = _ACTION or actionName
+	   end
+	end
