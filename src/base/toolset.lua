392942ed0ca87db5f0d99d4f15e31173ef217491
Richard Geary <richardg.work@gmail.com>
2012-07-26 10:41:36 -0400

    Remove global map fn
    --HG--
    branch : all

--- src/base/toolset.lua
+++ src/base/toolset.lua
@@ -10,16 +10,8 @@
 --
 	premake.Toolset.allToolsets = {}
 	
-	function map(table,fn)
-	  rv = {}
-	  for key,value in pairs(table) do
-	  	rv = rv + fn(key,value)
-	  end
-	  return rv
-	end
-	
 	function premake.Toolset.getToolsetNames()
-	    --toolsetNames = map(Toolset.allToolsets, function(k,v) return k; end)
+	    local toolsetNames = map(Toolset.allToolsets, function(k,v) return k; end)
 	    local toolsetNames = {}
 		for k,v in pairs(premake.Toolset.allToolsets) do
 			table.insert(toolsetNames, k)
