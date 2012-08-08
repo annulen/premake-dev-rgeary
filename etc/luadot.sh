bb2b5bff5eb30fcaf1252a1b8629d07c48e1bf66
Richard Geary <richardg.work@gmail.com>
2012-08-08 09:41:48 -0400

    Fix for bash scripts
    --HG--
    branch : all

--- etc/luadot.sh
+++ etc/luadot.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# find ~/inet/premake-dev-ninja/src -name '*.lua' | xargs sed -n "s/$1\./\n$1\./gp" | sed 's/[^a-zA-Z.].*//' | sort -u
-find ~/inet/premake-dev-ninja/src -name '*.lua' | xargs sed -n "s/\($1\.[_a-zA-Z0-9]*\)/\n\1\n/gp" | grep "^$1" | sort -u
+DIR=$(dirname $(readlink -f "$0"))/..
+# find $DIR/src -name '*.lua' | xargs sed -n "s/$1\./\n$1\./gp" | sed 's/[^a-zA-Z.].*//' | sort -u
+find $DIR/src -name '*.lua' | xargs sed -n "s/\($1\.[_a-zA-Z0-9]*\)/\n\1\n/gp" | grep "^$1" | sort -u
